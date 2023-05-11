acd58e9bb51045229ad62477971878ca24030a99
Xander1936 <alexandrehervemassodamambeleck@gmail.com>
2023-05-11 17:36:06 +0100

    About Us Section

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -16,10 +16,10 @@ const Navbar = () => {
         <img src={images.gericht} alt="app logo" />
       </div>
       <ul className='app__navbar-links'>
-        <li className='p__opensans'><a href='#home'>Home</a></li>
-        <li className='p__opensans'><a href='#about'>About</a></li>
+        <li className='p__opensans'><a href='#home' id='home'>Home</a></li>
+        <li className='p__opensans'><a href='#about' >About</a></li>
         <li className='p__opensans'><a href='#menu'>Menu</a></li>
-        <li className='p__opensans'><a href='#awards'>Awards</a></li>
+        <li className='p__opensans'><a href='#awards' >Awards</a></li>
         <li className='p__opensans'><a href='#contact'>Contact</a></li>
       </ul>
       <div className='app__navbar-login'>
@@ -34,11 +34,11 @@ const Navbar = () => {
         <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>
           <MdOutlineRestaurantMenu fontSize={27} className='overlay__close' onClick={() => setToggleMenu(false)} />
           <ul className='app__navbar-smallscreen-links'>
-            <li className='p__opensans'><a href='#home'>Home</a></li>
-            <li className='p__opensans'><a href='#about'>About</a></li>
-            <li className='p__opensans'><a href='#menu'>Menu</a></li>
-            <li className='p__opensans'><a href='#awards'>Awards</a></li>
-            <li className='p__opensans'><a href='#contact'>Contact</a></li>
+            <li className='p__opensans'><a href='#home' onClick={() => setToggleMenu(false)} id='home'>Home</a></li>
+            <li className='p__opensans'><a href='#about' onClick={() => setToggleMenu(false)} id='about'>About</a></li>
+            <li className='p__opensans'><a href='#menu' onClick={() => setToggleMenu(false)}>Menu</a></li>
+            <li className='p__opensans'><a href='#awards' onClick={() => setToggleMenu(false)}>Awards</a></li>
+            <li className='p__opensans'><a href='#contact' onClick={() => setToggleMenu(false)}>Contact</a></li>
           </ul>
         </div>
         )}
