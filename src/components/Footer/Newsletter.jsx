a14e14ed8072647d54511fe18f66f66ba35e7f96
Xander1936 <alexandrehervemassodamambeleck@gmail.com>
2023-05-19 03:16:32 +0100

    footer section

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -7,6 +7,12 @@ const Newsletter = () => (
   <div className='app__newsletter'>
       <div className='app__newsletter-heading'>
         <SubHeading title='Newsletter' />
+        <h1 className='headtext__cormorant'>Subscribe To Our Newsletter</h1>
+        <p className='p__opensans' style={{marginBottom: '2rem'}}>And never miss latest Updates!</p> 
+      </div>
+      <div className='app__newsletter-input flex__center'>
+        <input type='email' placeholder='Enter Your Email Address'/>
+        <button type='button' className='custom__button'>Subscribe</button>
       </div>
   </div>
 );
